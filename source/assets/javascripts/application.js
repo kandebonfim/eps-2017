aa21dc0782725dedb5836eb3e695b90657757856
kandebonfim <kandebonfim@gmail.com>
2017-10-12 21:45:59 -0300

    Implement lateral navigation

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -7,3 +7,29 @@ if ('addEventListener' in document) {
 		FastClick.attach(document.body);
 	}, false);
 }
+
+window.addEventListener('scroll', (function(event) {
+  var d, e, g, sessionLink, progressBar, targets, w, y;
+  w = window;
+  d = document;
+  e = d.documentElement;
+  g = d.getElementsByTagName('body')[0];
+  y = w.innerHeight || e.clientHeight || g.clientHeight;
+  targets = document.querySelectorAll('.js-scrollable-session');
+  sessionLink = document.querySelectorAll('.js-session-link');
+  targets.forEach(function(el, index) {
+    var progress;
+    progress = (el.getBoundingClientRect().top - y) / el.getBoundingClientRect().height;
+    progress = progress * -1;
+    progress = Math.max(0, progress);
+    console.log(progress);
+    progressBar = sessionLink[index].querySelector('.nav__bar');
+    if (progress > 0.5 && progress < 1.5) {
+      sessionLink[index].classList.add('active');
+      progressBar.style.width = progress * 1.5 + "em";
+    } else {
+      sessionLink[index].classList.remove('active');
+      progressBar.style.width = "0em";
+    }
+  });
+}), false);
